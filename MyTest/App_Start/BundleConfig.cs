55713e28427ed9a9fce949705aefb0a0f6b67087
visualstudiome8@gmail.com <visualstudiome8@gmail.com>
2020-09-18 15:10:39 +0300

    Добавьте файлы проекта.

---
+++ MyTest/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MyTest
+{
+    public class BundleConfig
+    {
+        // Дополнительные сведения об объединении см. на странице https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                      "~/Scripts/jquery-3.5.1.min.js",
+                      "~/Scripts/jquery.unobtrusive-ajax*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                      "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ MyTest/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using MyTest.ProxyClass;
+
+using Newtonsoft.Json;
+
+
+namespace MyTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        IICUTechservice techservice = new IICUTechservice();
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        { 
+
+            string res = techservice.Login(email, password, null);
+            Dictionary<string, string> loginResult = JsonConvert.DeserializeObject<Dictionary<string, string>>(res);
+            if (loginResult.ContainsKey("ResultCode") && loginResult.ContainsValue("-1"))
+            {
+                return PartialView("Error", loginResult);
+            }
+            else
+            {
+                return PartialView("Response", loginResult);
+            }
+        }
+
+
+
+    }
+}
